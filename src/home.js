02364b56dc0b1d98e6c7ded5e3c8861673166511
leisingha <120764124+leisingha@users.noreply.github.com>
2025-03-26 21:37:12 -0400

    create home module

---
+++ src/home.js
@@ -0,0 +1,38 @@
+import imgImport from "./jokebear-munch.gif";
+
+function populateHomeContent(){
+    const content = document.querySelector('#content');
+    const sectionIntro = document.createElement('section');
+    sectionIntro.classList.add('intro');
+    const sectionMiddle = document.createElement('section');
+    sectionIntro.classList.add('middle');
+    const sectionEnding = document.createElement('section');
+    sectionIntro.classList.add('ending');
+
+    const h2 = document.createElement('h2');
+    h2.textContent = 'Welcome to Porkie Bun! 🐷';
+    const pIntro = document.createElement('p');
+    pIntro.textContent = `Are you ready to embark on a delicious journey filled with fluffy buns and savory surprises? \n Whether you're here to learn, laugh, or just drool over pictures of pork buns, you're in the right place!`
+    sectionIntro.appendChild(h2);
+    sectionIntro.appendChild(pIntro);
+
+    
+    const img = document.createElement('img');
+    img.src = imgImport;
+    sectionMiddle.appendChild(img);
+    const div = document.createElement('div');
+    div.classList.add('fact-box');
+    const pMiddle = document.createElement('p');
+    pMiddle.textContent = `Fun fact: Did you know pork buns are scientifically proven* to make your day 100% better? \n (*Okay, maybe not scientifically, but trust us, they're magical.)`
+    div.appendChild(pMiddle);
+    sectionMiddle.appendChild(div);
+
+    const pEnding = document.createElement('p')
+    pEnding.textContent = `So grab a napkin (you'll need it), and let's dive into the world of pork buns! 🥟✨`
+    sectionEnding.appendChild(pEnding);
+
+    content.appendChild(sectionIntro);
+    content.appendChild(sectionMiddle);
+    content.appendChild(sectionEnding);
+    
+}
